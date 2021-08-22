e8d1569b3347dabf0985a1811be7380a44114b1b
JorgeGaston <gastonpuig@gmail.com>
2021-08-22 18:52:18 +0200

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,8 +1,8 @@
-import os
 import psutil
 import datetime
 import time
-
+import os
+import pandas as pd
 
 class Metrics:
 
@@ -20,6 +20,16 @@ class Metrics:
         self.rx_bytes = 0
         self.failed_logins = 0
         self._last_failed_login_time = -1
+        self._last_tx_bytes = -1
+        self._last_rx_bytes = -1
+
+        #get metrics once, to initialize previous variables
+        self.get_metrics()
+
+        self.header = "timestamp,num_process,num_fds,num_conn,num_ssh,num_active_users,cpu_usage,cpu_load,ram,tx_bytes,rx_bytes,failed_logins"
+        self.columns = self.header.split(',')
+
+        self.file = None
 
     def _get_timestamp(self):
 
@@ -69,13 +79,27 @@ class Metrics:
 
     def _get_tx_bytes(self):
         with open("/sys/class/net/eth0/statistics/tx_bytes", "r") as a_file:
-            self.tx_bytes = int(a_file.read())
+
+            tx_bytes = int(a_file.read())
+            self.tx_bytes = 0
+            if (self._last_tx_bytes != -1):
+                if (tx_bytes > self._last_tx_bytes):
+                    self.tx_bytes = tx_bytes - self._last_tx_bytes
+
+            self._last_tx_bytes = tx_bytes
 
         #print("tx bytes = " + str(self.tx_bytes))
 
     def _get_rx_bytes(self):
         with open("/sys/class/net/eth0/statistics/rx_bytes", "r") as a_file:
-            self.rx_bytes = int(a_file.read())
+
+            rx_bytes = int(a_file.read())
+            self.rx_bytes = 0
+            if (self._last_rx_bytes != -1):
+                if (rx_bytes > self._last_rx_bytes):
+                    self.tx_bytes = rx_bytes - self._last_rx_bytes
+
+            self._last_rx_bytes = rx_bytes
 
         #print("rx bytes = " + str(self.rx_bytes))
 
@@ -128,18 +152,22 @@ class Metrics:
         self._get_rx_bytes()
         self._get_failed_logins()
 
-    def save_metrics(self, file):
+    def save_metrics(self, file = None):
 
         ''' Save metrics in a file '''
+        if file == None:
+            save_file = self.file
+        else:
+            save_file = file
 
-        with open(file, "a") as a_file:
-            a_file.write("\n")
+        with open(save_file, "a") as a_file:
 
             metrics = str(self.timestamp) + "," + str(self.num_process) + "," + str(self.num_fds) + "," + str(self.num_conn) + "," + str(self.num_ssh) + "," +\
                       str(self.num_active_users) + "," + str(self.cpu_usage) + "," + str(self.cpu_load) + "," + str(self.ram) + "," +\
                       str(self.tx_bytes) + "," + str(self.rx_bytes) + "," + str(self.failed_logins)
 
             a_file.write(metrics)
+            a_file.write("\n")
 
 
     def print_metrics(self):
@@ -151,14 +179,38 @@ class Metrics:
 
         print (metrics)
 
-    def collect_system_information(self, file):
+    def collect_system_information(self, log_file):
 
         '''
         This method should retreive the system information and store it in a file when
         an anomaly is detected. The file with the data may be attached to the email notification
         '''
+        with open(log_file, "a") as a_file:
+            a_file.write("System information should be here")
 
-        pass
+
+
+    def initialize_data_file(self, data_file):
+
+
+        #if file exists we should not replace it. Otherwise, we can create it
+        if os.path.isfile(data_file) == False:
+            with open(data_file, "a") as a_file:
+                a_file.write(self.header)
+                self.file = data_file
+
+
+    def return_metrics(self):
+
+
+
+        data = [[ self.timestamp, self.num_process, self.num_fds, self.num_conn, self.num_ssh, self.num_active_users, self.cpu_usage, \
+                self.cpu_load, self.ram, self.tx_bytes, self.rx_bytes, self.failed_logins ]]
+
+        dframe = pd.DataFrame(data, columns=self.columns)
+
+        return dframe
+
 
 
 if (__name__ == '__main__'):
@@ -166,13 +218,14 @@ if (__name__ == '__main__'):
 
     data_file = "collected_data.csv"
 
-    with open(data_file, "a") as a_file:
-        a_file.write("timestamp,num_process,num_fds,num_conn,num_ssh,num_active_users,cpu_usage,cpu_load,ram,tx_bytes,rx_bytes,failed_logins")
-
+#    with open(data_file, "a") as a_file:
+ #       a_file.write(metrics.header)
+    print(metrics.header)
+    print(metrics.return_metrics())
     #get data each 20 seconds, during one day
-    num_samples = 24 * 60 * 60 // 20
+    #num_samples = 24 * 60 * 60 // 20
 
-    for i in range(0, num_samples):
-        metrics.get_metrics()
-        metrics.save_metrics(data_file)
-        time.sleep(20)
+    #for i in range(0, num_samples):
+    #    metrics.get_metrics()
+    #    metrics.save_metrics(data_file)
+    #    time.sleep(20)
